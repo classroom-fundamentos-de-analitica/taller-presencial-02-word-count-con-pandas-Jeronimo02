bbc7cef71ece6b303255f1f4d3823c6798e86fe2
Jeronimo <111191077+Jeronimo02@users.noreply.github.com>
2024-02-20 22:36:54 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -4,6 +4,7 @@ import glob
 
 import pandas as pd
 
+import string
 
 def load_input(input_directory):
     """Load text files in 'input_directory/'"""
@@ -12,21 +13,39 @@ def load_input(input_directory):
     # un DataFrame de Pandas. Cada línea del archivo de texto debe ser una
     # entrada en el DataFrame.
     #
-
-
+    
+    filenames = glob.glob(input_directory + '/*.txt')
+    dataframes = pd.concat((pd.read_csv(f, sep='\t', header=None, names=['text']) for f in filenames), ignore_index=True)
+    
+    return dataframes
+    
+    
+    
 def clean_text(dataframe):
     """Text cleaning"""
     #
     # Elimine la puntuación y convierta el texto a minúsculas.
     #
+    
+    dataframe['clean_text'] = dataframe['text'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)).lower())
+    return dataframe
+    
 
 
 def count_words(dataframe):
     """Word count"""
-
+    words = dataframe['clean_text'].str.split(expand=True).stack()
+    
+    wordCounts = words.value_counts().reset_index()
+    wordCounts.columns = ['word', 'count']
+    
+    return wordCounts
 
 def save_output(dataframe, output_filename):
     """Save output to a file."""
+    
+    dataframe.to_csv(output_filename, sep='\t', index=False, header=False)
+
 
 
 #
@@ -34,6 +53,11 @@ def save_output(dataframe, output_filename):
 #
 def run(input_directory, output_filename):
     """Call all functions."""
+    dataframe = load_input(input_directory)
+    cleanDataframe = clean_text(dataframe)
+    WordCountDataframe = count_words(cleanDataframe)
+    
+    save_output(WordCountDataframe, output_filename)
 
 
 if __name__ == "__main__":
